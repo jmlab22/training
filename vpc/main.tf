b8aa2acc20d6c417e1b8e241d13bbab2c8804982
jm.lab <jm@lab.com>
2018-10-08 15:18:58 +0200

    correct subnet AZ

--- vpc/main.tf
+++ vpc/main.tf
@@ -11,8 +11,9 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "mysubnet-a" {
-  vpc_id     = "${aws_vpc.main.id}"
-  cidr_block = "172.23.1.0/24"
+  vpc_id            = "${aws_vpc.main.id}"
+  cidr_block        = "172.23.1.0/24"
+  availability_zone = "eu-west-1a"
 
   tags {
     Name = "mysubnet-a"
