dbf2edd98fd5a4265e82569847bc3c5a6da7d675
jm.lab <jm@lab.com>
2018-10-10 09:44:49 +0200

    delete old files

--- vpc/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "my-bucket-tfstate"
-    key    = "vpc/terraform.tfstate"
-    region = "eu-west-1"
-  }
-}
--- webapps/backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "my-bucket-tfstate"
-    key    = "webapps/terraform.tfstate"
-    region = "eu-west-1"
-  }
-}
